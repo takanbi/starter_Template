90bcea19e4008d00de4d74784d3b1c999ec4baaa
takanbi <tunji.akanbi@century.edu>
2023-03-31 11:35:58 -0500

    added test gsap animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
+gsap.registerPlugin(GSDevTools, SplitText, TextPlugin, ScrollTrigger);
+//jQuery
+$(function () {
+  // jQuery methods go here...
 
-//jQuery 
-$(function(){
-
-    // jQuery methods go here...
-  
+  let wrapper0 = $("#wrapper0");
+  let heading0 = $("#heading0");
+  let split0;
+  let animation0 = gsap.timeline({
+    id: "animation0",
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 0.3,
   });
+  function init0() {
+    gsap.set(wrapper0, {autoAlpha: 1});
+    split0 = new SplitText(heading0, {type: "chars"})
+    animation0.from(split0.chars, {
+      opacity: 0,
+      y: 50,
+      ease: "back(4)",
+      stagger: 0.05
+    },"+=2");
+    GSDevTools.create({
+      animation0: "animation0"
+    });
+  }
+window.addEventListener("load", init0);
+  // tl.from(heading, {opacity: 0, duration: 10});
+});
